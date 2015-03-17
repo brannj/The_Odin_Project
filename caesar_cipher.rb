7dd35d0ac23267fb2374ab22fe1cb85f6b790225
brann <brann3james@gmail.com>
2015-03-17 20:45:26 +0700

    caesar cipher - The Odin Project - Task One

---
+++ caesar_cipher.rb
@@ -0,0 +1,36 @@
+# Caesar Cipher Task - The Odin Project
+
+=begin
+Task: take a string, return a cipher.
+
+Method: Cycles through input text with #each_char.
+        Uses #ord to convert to ASCII decimal.
+        Checks against ASCII A..Z and a..z ranges.
+        Wraps back to A or a if goes past Z or z.
+        Uses #chr to convert to letter.
+=end
+
+def caesar_cipher(text, shift_factor)
+  cipher = ""
+  text.each_char do |letter|
+    cipher << case
+    when (65..90).include?(letter.ord)
+      if (letter.ord + shift_factor) > 90
+        (64 + (shift_factor - (90 - letter.ord))).chr
+      else
+        (letter.ord + shift_factor).chr
+      end
+    when (97..122).include?(letter.ord)
+      if (letter.ord + shift_factor) > 122
+        (96 + (shift_factor - (122 - letter.ord))).chr
+      else
+        (letter.ord + shift_factor).chr
+      end
+    else
+      letter
+    end
+  end
+  puts cipher
+end
+
+caesar_cipher("What a string!", 5)
---
+++ stock_picker.rb
@@ -0,0 +1,18 @@
+# Stock Picker Task - The Odin Project
+
+=begin
+Task: Take an array of numbers,
+      return the index of the smallest and largest.
+      Largest index cannot be less than smallest index.
+      Therefore largest cannot be first and smallest cannot be last.
+
+Method: Uses an iteration within an iteration.
+        Define a variable to hold subtraction results.
+        Subtract 0..-2 from 1..-1 then 1..-2 from 2..-1, etc.
+        If result is > than stored value make it stored value and
+        store the indexes of each value in an array variable.
+=end
+
+def stock_picker
+
+end
