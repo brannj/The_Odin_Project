8124a49af47f1dd095ad4916a9ed0e90880eb941
brann <brann3james@gmail.com>
2015-04-21 13:37:33 +0700

    Initial event manager.

---
+++ serialization/event_manager/lib/event_manager.rb
@@ -0,0 +1,61 @@
+require "csv"
+require "sunlight/congress"
+require "erb"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
+
+def save_thank_you_letters(id, form_letter)
+  Dir.mkdir("output") unless Dir.exists? "output"
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, "w") do |file|
+    file.puts form_letter
+  end
+end
+
+def clean_phone_number(home_phone)
+  home_phone.gsub!(/[^0-9]/, "").join
+#  if home_phone.length < 10 || home_phone.length > 11
+#    "Invalid number."
+#  elsif home_phone[0] != 1 && home_phone.length == 11
+#    "Invalid number."
+#  elsif home_phone.length == 11
+#    home_phone[1..10]
+#  else
+#    "Invalid number."
+#  end
+end
+
+puts "Event Manager Initialized!"
+
+contents = CSV.open "event_attendees.csv",
+            headers: true, header_converters: :symbol
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  home_phone = clean_phone_number(row[:homephone])
+
+#  legislators = legislators_by_zipcode(zipcode)
+
+#  form_letter = erb_template.result(binding)
+
+#  save_thank_you_letters(id, form_letter)
+
+  puts "#{name.capitalize} #{home_phone}"
+end
