edb1658d1f0dff4f4a4b2195b94b31d35f3624ee
brann <brann3james@gmail.com>
2015-04-22 10:36:27 +0700

    Add time and day of week targeting report.

--- serialization/event_manager/lib/event_manager.rb
+++ serialization/event_manager/lib/event_manager.rb
@@ -22,6 +22,16 @@ def save_thank_you_letters(id, form_letter)
   end
 end
 
+def save_registration_report(form_targeting)
+  Dir.mkdir("report") unless Dir.exists? "report"
+
+  filename = "report/reg_report_#{Time.now.strftime('%d-%m-%y')}.html"
+
+  File.open(filename, "w") do |file|
+    file.puts form_targeting
+  end
+end
+
 def clean_phone_number(home_phone)
   home_phone = home_phone.to_s.gsub(/[^\d]/, "")
   if home_phone.length == 11 && home_phone[0] != 1
@@ -33,25 +43,56 @@ def clean_phone_number(home_phone)
   end
 end
 
+def date_parser(reg_date)
+  DateTime.strptime(reg_date, '%m/%d/%y %H:%M')
+end
+
+def generate_thank_you_letters(contents)
+
+  template_letter = File.read "form_letter.erb"
+  erb_template = ERB.new template_letter
+
+  contents.each do |row|
+    id = row[0]
+    name = row[:first_name]
+
+    zipcode = clean_zipcode(row[:zipcode])
+
+    legislators = legislators_by_zipcode(zipcode)
+
+    form_letter = erb_template.result(binding)
+
+    save_thank_you_letters(id, form_letter)
+  end
+end
+
+def mobile_alerts
+  contents.each do |row|
+    id = row[0]
+    name = row[:first_name]
+
+    home_phone = clean_phone_number(row[:homephone])
+  end
+end
+
 puts "Event Manager Initialized!"
 
 contents = CSV.open "event_attendees.csv",
             headers: true, header_converters: :symbol
 
-template_letter = File.read "form_letter.erb"
-erb_template = ERB.new template_letter
+template_report = File.read "form_targeting.erb"
+erb_template = ERB.new template_report
+
+reg_times = Hash.new(0)
+reg_days = Hash.new(0)
 
 contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
+  reg_date = date_parser(row[:regdate])
 
-  zipcode = clean_zipcode(row[:zipcode])
+  reg_times[reg_date.strftime("%l %P")] += 1
+  reg_days[reg_date.strftime("%a")] += 1
 
-  home_phone = clean_phone_number(row[:homephone])
+  form_targeting = erb_template.result(binding)
 
-  legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letters(id, form_letter)
+  save_registration_report(form_targeting)
 end
