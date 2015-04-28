1c88021be1ec875dafd0e561526c01c4605f5fb4
brann <brann3james@gmail.com>
2015-04-28 14:53:41 +0700

    Add save, load and quit functionality.

--- serialization/hangman/lib/hangman.rb
+++ serialization/hangman/lib/hangman.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 # Add a save game option to turns.
 # Add a load game option to the initialisation.
 class Hangman
@@ -6,7 +7,7 @@ class Hangman
     @word = select_word
     @guesses = []
     @progress = Array.new(@word.length, " _")
-    start
+    welcome
   end
 
   def select_word
@@ -16,9 +17,16 @@ class Hangman
     dictionary.sample.downcase.strip.split("")
   end
 
+  def welcome
+    puts "\nWould you like to load an existing game or start a new game?"
+    print "Type load or new: "
+    game_options(gets.chomp.downcase)
+  end
+
   def start
-    puts "Welcome. Try to guess the word by guessing letters."
+    puts "\nTry to guess the word by guessing letters."
     puts "If you make six incorrect guesses you lose."
+    puts "Type 'save' to save the game and 'quit' to exit the game."
     until @guesses.size == 6 || !@progress.include?(" _")
       play
     end
@@ -27,12 +35,10 @@ class Hangman
 
   def play
     print "\n"
-    @guess = make_guess
-
-    check_letter
-
     puts @progress.join
     print_guesses
+    @guess = make_guess
+    check_letter
   end
 
   def check_letter
@@ -41,27 +47,48 @@ class Hangman
         @progress[position] = " #{letter}" if @guess == letter
       end
     else
-      @guesses << @guess
+      @guesses << @guess unless @guess.nil?
     end
   end
 
   def save_game
     Dir.mkdir("saved_games") unless Dir.exists? "saved_games"
-    save = File.new("hangman_save.yaml")
-    yaml = YAML::dump(self)
-    save.write(yaml)
+    save_file = File.new("saved_games/hangman.yaml", "w")
+    save = YAML::dump(self)
+    save_file.write(save)
+    save_file.close
+    puts "Game saved."
   end
 
   def load_game
+    load_file = File.open("saved_games/hangman.yaml", "r")
+    load_game = load_file.read
+    YAML::load(load_game).start
+  end
 
+  def quit_game
+    puts "Goodbye.\n"
+    exit
+  end
+
+  def game_options(choice)
+    case choice
+    when "load"
+      load_game
+    when "new"
+      start
+    else
+      puts "That's not an option."
+      welcome
+    end
   end
 
   def print_guesses
-    puts "Incorrect: #{@guesses.join(", ")}" unless @guesses.empty?
+    puts "Incorrect (#{6 - @guesses.size} left): #{@guesses.join(", ")}" unless @guesses.empty?
   end
 
   def solution
-    puts " #{@word.join(' ')}" unless @guesses.size < 6
+    puts " #{@word.join(' ')}"
   end
 
   def make_guess
@@ -72,6 +99,8 @@ class Hangman
   def guess_parser(guess)
     if guess == "save"
       save_game
+    elsif guess == "quit"
+      quit_game
     elsif @guesses.include?(guess)
       puts "You have already tried that letter."
       make_guess
