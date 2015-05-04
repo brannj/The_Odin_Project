8f2c45d74206d634fef0715fe0b08ecd2174787f
brann <brann3james@gmail.com>
2015-05-04 19:13:56 +0700

    add README.

--- recursion/README.md
+++ recursion/README.md
@@ -1,3 +1,9 @@
-# Projects from The Odin Project
+# Project: Recursion
 
-This repository contains projects completed while working through [The Odin Project](www.theodinproject.com).
+## Warmup: Fibonacci
+**Task Summary:** Write two methods to output n numbers of the fibonacci sequence. First using iteration, then using recursion. [Full details here.](http://www.theodinproject.com/ruby-programming/recursion?ref=lnav)
+**Notes:** Both methods return n members counting from 1. 
+___
+
+## Project: Merge Sort
+**Task Summary:** Write a recursive merge sort method. [Full details here.](http://www.theodinproject.com/ruby-programming/recursion?ref=lnav)
--- recursion/fibonacci.rb
+++ recursion/fibonacci.rb
@@ -1,25 +1,22 @@
-# fibonacci number n = fibonacci number n-1 + fibonacci number n-2.
-# both solutions start counting from 0.
-
 def fibs(n)
   seq = [0, 1]
-  return "0" if n == 0            # replace with 0 with 1 to start count from 1
+  return "0" if n == 1            # replace with 1 with 0 to count from 0
 
-  while seq.length <= n           # replace <= with < to start count from 1
+  while seq.length < n           # replace < with <= to count from 0
     seq << seq[-1] + seq[-2]
   end
   seq.join(", ")
 end
 
-puts fibs(10)
+puts fibs(11)
 
 
 def fibs_rec(n, seq = [0, 1])
-  return [0] if n == 0            # replace with 0 with 1 to start count from 1
-  return seq.join(", ") if n == 1 # replace with 1 with 2 to start count from 1
+  return [0] if n == 1            # replace with 1 with 0 to count from 0
+  return seq.join(", ") if n == 2 # replace with 2 with 1 to count from 0
   seq << seq[-1] + seq[-2]
   fibs_rec(n - 1, seq)
   seq.join(", ")
 end
 
-puts fibs_rec(10)
+puts fibs_rec(11)
