7bebbaed81dd8636310dd9f96dd48a05454007f5
brann <brann3james@gmail.com>
2015-05-05 13:46:58 +0700

    recursive with loop solution

--- recursion/merge_sort.rb
+++ recursion/merge_sort.rb
@@ -1,16 +1,11 @@
 def merge_sort(list)
-  # if list size is 1 or 0 it is already sorted.
   return list if list.size <= 1
-  # split the list in half and use recursoin on the half list.
-  # this will lead to lists of individual elements due to the above return.
+
   half_list = merge_sort(list.slice!(0..(list.size / 2 - 1)))
-  # Do the same with the other half of the list.
   other_half_list = merge_sort(list)
-  # While there are elements in either list
-  # compare the first elements in each list
-  # move the smalller to the sorted list
-  # repeat until no more lists to merge
+
   sorted = []
+
   until half_list.empty? || other_half_list.empty?
     if half_list[0] < other_half_list[0]
       sorted.push(half_list.shift)
@@ -18,15 +13,11 @@ def merge_sort(list)
       sorted.push(other_half_list.shift)
     end
   end
-  # push any element that remains in the list to empty it.
-  until half_list.empty?
-    sorted.push(half_list.shift)
-  end
 
-  until other_half_list.empty?
-    sorted.push(other_half_list.shift)
-  end
+  sorted.concat(half_list)
+  sorted.concat(other_half_list)
+
   sorted
 end
 
-p merge_sort([4,76,9,0,23,1,5,99,32,53])
+p merge_sort([1, 2, 4, 3, 10, 9, 8, 7, 6])
