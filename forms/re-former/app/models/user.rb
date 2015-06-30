2effc4f348a98863942edb413498e8dbfe3b3089
brann <brann3james@gmail.com>
2015-06-30 19:33:17 +0700

    Add authenticity token.

--- forms/re-former/app/models/user.rb
+++ forms/re-former/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true
-  validates :password, presence: true, length: { minumum: 12 }
+  validates :password, presence: true
 end
