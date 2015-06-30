96b616d3e2e5d89c5007fff2c3a371a2651b19e5
brann <brann3james@gmail.com>
2015-06-30 14:27:30 +0700

    Add users routes.

--- forms/re-former/app/models/user.rb
+++ forms/re-former/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minumum: 12 }
 end
