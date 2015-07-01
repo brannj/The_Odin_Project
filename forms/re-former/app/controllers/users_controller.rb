81527f69fe5ea8a0d68213b259c1a782d2bffb2e
brann <brann3james@gmail.com>
2015-07-01 19:31:42 +0700

    Add set_user helper.

--- forms/re-former/app/controllers/users_controller.rb
+++ forms/re-former/app/controllers/users_controller.rb
@@ -1,16 +1,14 @@
 class UsersController < ApplicationController
-    # before_action :set_user, only: [:edit]
+  before_action :set_user, only: [:edit, :update]
 
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def create
-    # @user = User.new(name: params[:name], email: params[:email], password: params[:password])
     @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
@@ -20,7 +18,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to new_user_path
     else
@@ -28,9 +25,9 @@ class UsersController < ApplicationController
     end
   end
 
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
