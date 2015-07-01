28d41687ab5f67d0e2ddfe62353fc74b97f46187
brann <brann3james@gmail.com>
2015-07-01 19:03:16 +0700

    Add edit page.

--- forms/re-former/app/controllers/users_controller.rb
+++ forms/re-former/app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+    # before_action :set_user, only: [:edit]
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -12,6 +18,19 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
