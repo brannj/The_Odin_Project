43075e950ffd94d7f319a5bc386a61e35e976223
brann <brann3james@gmail.com>
2015-07-17 16:14:22 +0700

    Add framework for posts.

--- authentication/members_only/config/routes.rb
+++ authentication/members_only/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'posts#index'
   get     'login'  => 'sessions#new'
   post    'login'  => 'sessions#create'
   delete  'logout' => 'sessions#destroy'
   resources :users, only: [:show, :edit, :update, :destroy]
-
+  resources :posts, only: [:new, :create, :index]
 end
