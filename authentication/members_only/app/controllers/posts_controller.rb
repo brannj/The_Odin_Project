27a0554ce878bb1f1f26e0931efe8fc6ddd21f94
brann <brann3james@gmail.com>
2015-07-20 18:47:22 +0700

    small styling changes.

--- authentication/members_only/app/controllers/posts_controller.rb
+++ authentication/members_only/app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create]
 
   def index
-    @posts = Post.all
+    @posts = Post.paginate(page: params[:page])
   end
 
   def show
